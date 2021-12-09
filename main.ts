7a44dc17736e6a696a025a845f0992718597af4c
Txominbalza <95851164+Txominbalza@users.noreply.github.com>
2021-12-09 14:13:43 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,36 @@
+let tool = 0
+let Puntuazioa = 0
+input.onButtonPressed(Button.A, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (tool == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    Puntuazioa = 0
+    basic.pause(1000)
+    basic.showString("A")
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Puntuazioa)
+    basic.pause(1000)
+    basic.clearScreen()
+})
